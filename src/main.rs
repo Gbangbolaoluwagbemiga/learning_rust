df26ce313595fa8a84fe6842e18418b7a1db1074
Oluwagbemiga <oluwagbemigagbangbola@gmail.com>
2025-05-31 17:46:19 +0100

    strings

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    //     println!("Hello, world!");
 
-    let t: i32 = 5;
-    let t: i32 = t + 1;
-    println!("t: {t}");
+    //     let t: i32 = 5;
+    //     let t: i32 = t + 1;
+    //     println!("t: {t}");
+
+    // STRINGS
+    let mut name = String::from("Gbenga philip");
+    name.push_str("philip");
+    println!("My name is : {name}");
+
+    let men = "phil me";
+    // name.push_str("gbenga");
+    print!("name: {men}");
+    // name = "gbenga"; // This will not work because `name` is immutable
 }
