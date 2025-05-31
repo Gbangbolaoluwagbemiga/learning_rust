5dd54de40cd8502093a65ba31f84d7452032fb5f
Oluwagbemiga <oluwagbemigagbangbola@gmail.com>
2025-05-31 18:14:18 +0100

    Refactor main function to include examples of arrays, vectors, and tuples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 fn main() {
     //     println!("Hello, world!");
 
@@ -13,5 +15,29 @@ fn main() {
     let men = "phil me";
     // name.push_str("gbenga");
     print!("name: {men}");
-    // name = "gbenga"; // This will not work because `name` is immutable
+    // name = "gbenga";
+
+    // Arrays
+    let arr = [1, 2, 3, 4, 5];
+    let arr2 = [arr[0]];
+    let one = arr[0];
+    println!("First element of the array: {}", one);
+    println!("Array: {:?}", arr);
+    println!("Array2: {:?}", arr2);
+
+    // Vectors
+    let vec = vec![1, 2, 3, 4, 5];
+    let first_element = vec[0];
+    let add = vec.clone();
+    print!("Added {:?}", add);
+    println!("First element of the vector: {:?}", first_element);
+    print!("Vector: {:?}", vec);
+
+    // Tuples
+    let tuple = ("Trial", 2, 3);
+    let types = tuple.0;
+    println!("Tuple first element: {}", types);
+    let (name, age, height) = tuple;
+    println!("Tuple: {:?}", tuple);
+    println!("Name: {}, Age: {}, Height: {}", name, age, height);
 }
