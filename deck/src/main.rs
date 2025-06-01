4e4d43301e9edfe5e3df82ccad7fa4775322900b
Oluwagbemiga <oluwagbemigagbangbola@gmail.com>
2025-06-01 21:23:29 +0100

    implementation for struct

--- deck/src/main.rs
+++ deck/src/main.rs
@@ -3,20 +3,26 @@ struct Deck {
     cards: Vec<String>,
 }
 
-fn main() {
-    let suits = ["Hearts", "Spades", "Diamonds"];
-    let values = ["Ace", "Two", "Three"];
-    let mut cards = vec![];
+impl Deck {
+    fn new() -> Self {
+        let suits = ["Hearts", "Spades", "Diamonds"];
+        let values = ["Ace", "Two", "Three"];
+        let mut cards = vec![];
 
-    for suit in suits {
-        for value in values {
-            let card = format!("{} of {}", value, suit);
-            cards.push(card);
-            // println!("my cards are :{:?}", cards);
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+                // println!("my cards are :{:?}", cards);
+            }
         }
-    }
 
-    let deck = Deck { cards: cards };
+        Deck { cards: cards }
+    }
+}
+
+fn main() {
+    let deck = Deck::new();
     print!("Here is my deck {:#?}", deck);
     // print!("Here is my deck cards {:?}", deck.cards)
 }
