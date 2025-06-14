c5d4b6a899e07a49843cb24329e203d1084b686c
Oluwagbemiga <oluwagbemigagbangbola@gmail.com>
2025-06-14 14:02:19 +0100

    testing

--- deck/src/main.rs
+++ deck/src/main.rs
@@ -27,18 +27,27 @@ impl Deck {
         self.cards.shuffle(&mut rng);
     }
 
-    fn add(&mut self) {
-        let newcard = String::from("Gbenga philip");
-        self.cards.push(newcard);
+    fn deal(&mut self, num_card: usize) -> Vec<String> {
+        self.cards.split_off(self.cards.len() - num_card)
     }
+
+    // fn add(&mut self) {
+    //     let newcard = String::from("Gbenga philip");
+    //     self.cards.push(newcard);
+    // }
 }
 
 fn main() {
-    let mut deck = Deck::new();
-    deck.shuffler();
-    print!("Here is my deck {:#?}", deck);
-    deck.shuffler();
-    deck.add();
-    print!("Here is my deck {:#?}", deck);
+    // let mut deck = Deck::new();
+    // deck.shuffler();
+    // print!("Here is my deck {:#?}", deck);
+
+    // print!("Here is my sliced card {:#?}", deck.cards.len());
+    let deal = deck.deal(3);
+
+    print!("Here is my sliced card {:#?}", deal);
+    // deck.shuffler();
+    // deck.add();
+    // print!("Here is my deck {:#?}", deck);
     // print!("Here is my deck cards {:?}", deck.cards)
 }
