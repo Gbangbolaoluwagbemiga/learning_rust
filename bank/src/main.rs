d9be46457c9c6a92787a480e779bf97c85a273f9
Oluwagbemiga <oluwagbemigagbangbola@gmail.com>
2025-06-17 14:11:13 +0100

    bnk completed, error handling to withdraw yet to be implemented

--- bank/src/main.rs
+++ bank/src/main.rs
@@ -1,5 +1,4 @@
 #[derive(Debug)]
-
 struct Bank {
     accounts: Vec<Account>,
 }
@@ -10,10 +9,18 @@ impl Bank {
     }
 
     fn add_account(&mut self, account: Account) {
-        self.accounts.push(&account);
+        self.accounts.push(account);
     }
 
-    // Add account
+    fn total_balance(&self) -> u32 {
+        self.accounts.iter().map(|account| account.balance).sum()
+    }
+    fn summary(&self) -> Vec<String> {
+        self.accounts
+            .iter()
+            .map(|account| account.summary())
+            .collect::<Vec<String>>()
+    }
 }
 
 #[derive(Debug)]
@@ -38,17 +45,28 @@ impl Account {
     fn withdraw(&mut self, amount: u32) {
         self.balance -= amount;
     }
+
+    fn summary(&self) -> String {
+        format!("{} has a balance {}", self.holder, self.balance)
+    }
 }
 
 fn main() {
     let mut bank = Bank::new();
     let mut account = Account::new(1, String::from("Oluwagbemiga"));
+    let mut account2 = Account::new(1, String::from("Xcel"));
 
-    bank.add_account(&account);
+    account.summary();
     account.deposit(500);
+    account2.deposit(300);
     account.withdraw(100);
 
     println!("{:?}", account);
+    bank.add_account(account);
+    bank.add_account(account2);
+    let bank_summary = bank.summary();
+    print!("your bank summary is {:#?}", bank_summary);
+    println!("Total balance {}", bank.total_balance());
 
-    println!("{:?}", bank);
+    println!("{:#?}", bank);
 }
