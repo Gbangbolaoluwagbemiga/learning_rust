9f01f8dafe57b57a4ff032df12bc3e7d8f07f3e0
Oluwagbemiga <oluwagbemigagbangbola@gmail.com>
2025-07-12 17:29:56 +0100

    lifetimes

---
+++ lifetimes/src/main.rs
@@ -0,0 +1,39 @@
+fn next_language<'a>(languages: &'a [String], current: &str) -> &'a str {
+    let mut found = false;
+
+    for lang in languages {
+        if found {
+            return lang;
+        }
+        if lang == current {
+            found = true;
+        }
+    }
+    languages.last().unwrap()
+}
+
+fn longest_str(languages: &[String]) -> String {
+    let mut len = String::new();
+
+    for longest in languages {
+        if longest.len() > len.len() {
+            len = longest.to_string();
+        } else {
+            len = len;
+        }
+    }
+
+    len
+}
+
+fn main() {
+    let languages = vec![
+        String::from("javascript"),
+        String::from("rust"),
+        String::from("solidity"),
+    ];
+    let result = next_language(&languages, "javascript");
+    let longest = longest_str(&languages);
+
+    println!("Longest string is {:?}", longest);
+}
