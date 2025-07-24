d7ba663f3a22be56b2f3f7fd7e17128b611829ae
Oluwagbemiga <oluwagbemigagbangbola@gmail.com>
2025-07-24 23:45:10 +0100

    init

--- exercises/src/main.rs
+++ exercises/src/main.rs
@@ -1,20 +1,73 @@
-use std::vec;
+// use std::vec;
 
-struct Point<T, U> {
-    x: T,
-    y: U,
+// struct Point<T, U> {
+//     x: T,
+//     y: U,
+// }
+
+// impl<T, U> Point<T, U> {
+//     fn x(&self) -> Vec<(&T, &U)> {
+//         return vec![(&self.x, &self.y)];
+//     }
+// }
+
+// fn main() {
+//     let p = Point { x: 5, y: 10 };
+//     let p2 = Point { x: 5.0, y: 10 };
+
+//     println!("p.x = {:?}", p.x());
+//     println!("p2.x = {:?}", p2.x());
+// }
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum ShirtColor {
+    Red,
+    Blue,
 }
 
-impl<T, U> Point<T, U> {
-    fn x(&self) -> Vec<(&T, &U)> {
-        return vec![(&self.x, &self.y)];
+struct Inventory {
+    shirts: Vec<ShirtColor>,
+}
+
+impl Inventory {
+    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+        user_preference.unwrap_or_else(|| self.most_stocked())
+    }
+
+    fn most_stocked(&self) -> ShirtColor {
+        let mut num_red = 0;
+        let mut num_blue = 0;
+
+        for color in &self.shirts {
+            match color {
+                ShirtColor::Red => num_red += 1,
+                ShirtColor::Blue => num_blue += 1,
+            }
+        }
+        if num_red > num_blue {
+            ShirtColor::Red
+        } else {
+            ShirtColor::Blue
+        }
     }
 }
 
 fn main() {
-    let p = Point { x: 5, y: 10 };
-    let p2 = Point { x: 5.0, y: 10 };
+    let store = Inventory {
+        shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
+    };
 
-    println!("p.x = {:?}", p.x());
-    println!("p2.x = {:?}", p2.x());
+    let user_pref1 = Some(ShirtColor::Red);
+    let giveaway1 = store.giveaway(user_pref1);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref1, giveaway1
+    );
+
+    // let user_pref2 = None;
+    // let giveaway2 = store.giveaway(user_pref2);
+    // println!(
+    //     "The user with preference {:?} gets {:?}",
+    //     user_pref2, giveaway2
+    // );
 }
